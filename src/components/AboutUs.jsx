96bb4a2c10ea9d4880a6fd1698e654d186d15b9c
farjana <farjanashaik153@gmail.com>
2024-06-10 08:44:06 +0530

    added about us and services page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -14,7 +14,7 @@ function AboutUs(){
             </div>
             <div id="aboutus_sub_heading">
                 <h2>We Make your Road Easier and Healthier</h2>
-                <p>At [Your Company Name], we're dedicated to transforming your daily commute and adventures into a journey of health and affordability. Our e-bikes are designed to provide a seamless and enjoyable ride, promoting a healthier lifestyle through increased physical activity. Not only do they help you stay active, but they also significantly reduce transportation costs. By choosing our e-bikes, you're opting for an eco-friendly, cost-effective solution that makes every road a little easier and a lot healthier. Join us in embracing a better way to travel—both for you and the planet.  </p>
+                <p>At E-Bike, we're dedicated to transforming your daily commute and adventures into a journey of health and affordability. Our e-bikes are designed to provide a seamless and enjoyable ride, promoting a healthier lifestyle through increased physical activity. Not only do they help you stay active, but they also significantly reduce transportation costs. By choosing our e-bikes, you're opting for an eco-friendly, cost-effective solution that makes every road a little easier and a lot healthier. Join us in embracing a better way to travel—both for you and the planet.  </p>
             <div id="aboutus_button">
                 <button>Contact us</button>
             </div>
